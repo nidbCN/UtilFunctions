d8f8e03d2d4d81bc603a134fa08580802c454aa8
Gaein nidb <mail@gaein.cn>
2024-10-21 01:02:25 +0800

    fix: use ContentResult

--- QrCode.cs
+++ QrCode.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -13,41 +14,27 @@ namespace UtilFunctions;
 public class QrCode(ILogger<QrCode> logger)
 {
     [Function("QrCode")]
-    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req, FunctionContext executionContext)
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
     {
         var content = req.Query["content"];
         if (string.IsNullOrEmpty(content))
-            return req.CreateResponse(HttpStatusCode.NotFound);
+            return new NotFoundResult();
 
-        logger.LogInformation("Received generate qrcode with: {content}.", content);
+        logger.LogInformation("Received generate qrcode with: {content}.", content!);
 
         using var generator = new QRCodeGenerator();
-        using var data = generator.CreateQrCode(content, QRCodeGenerator.ECCLevel.Q);
+        using var data = generator.CreateQrCode(content!, QRCodeGenerator.ECCLevel.Q);
         using var svg = new SvgQRCode(data);
 
         var svgStr = svg.GetGraphic(24)
             .Replace("\n", string.Empty);
 
-        logger.LogInformation("Response generated: SVG {svg}.", svgStr);
+        logger.LogInformation("Response generated: SVG length {len}.", svgStr.Length);
 
-        var resp = req.CreateResponse(HttpStatusCode.OK);
-        resp.Headers.Add("Content-Type", "image/svg+xml");
-        resp.WriteString(svgStr);
-
-        return resp;
-    }
-
-    [Function("Test")]
-    public static HttpResponseData RunTest([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req,
-        FunctionContext executionContext)
-    {
-        var logger = executionContext.GetLogger("Test");
-        logger.LogInformation("message logged");
-
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-        response.WriteString("Welcome to .NET isolated worker !!");
-
-        return response;
+        return new ContentResult()
+        {
+            Content = svgStr,
+            ContentType = "image/svg+xml"
+        };
     }
 }
